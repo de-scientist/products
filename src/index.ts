b427f2cc615a8062d459163b0d13ac1470c2c426
de-scientist <gitaumark502@gmail.com>
2025-10-28 19:27:57 +0300

    import the updateProduct function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 //importing express module
 import express from "express";
-import { createProduct, getProducts, getProduct } from "./controllers/user.ts";
+import { createProduct, getProducts, getProduct, updateProduct } from "./controllers/user.ts";
 import { PrismaClient } from "@prisma/client";
 
 //initialize express app
@@ -12,7 +12,8 @@ app.use(express.json());
 
 app.get("/products", getProducts);
 app.post("/products", createProduct);
-app.post("/products/:id", getProduct)
+app.post("/products/:id", getProduct);
+app.patch("/products/:id", updateProduct);
 
 //define a route handler for the listening root URL
 const PORT = 3000;
