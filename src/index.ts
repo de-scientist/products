5120e14524c520c3493736c2980de5e4ebb1d8b5
de-scientist <gitaumark502@gmail.com>
2025-10-28 14:33:10 +0300

    define a route handler for the listening root URL

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import express, { type Express, type Request, type Response } from "express";
 const app: Express = express();
 
 
-
+//define a route handler for the listening root URL
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
